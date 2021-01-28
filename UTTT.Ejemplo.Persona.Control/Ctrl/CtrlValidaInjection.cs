842807b9449f7b9d564ce46d622a05ce7d6f2180
Oscar Reyes <reyesmartinezoscar3g15@gmail.com>
2021-01-28 00:13:10 -0600

    Validación HTML funcionando

--- UTTT.Ejemplo.Persona.Control/Ctrl/CtrlValidaInjection.cs
+++ UTTT.Ejemplo.Persona.Control/Ctrl/CtrlValidaInjection.cs
@@ -10,7 +10,7 @@ namespace UTTT.Ejemplo.Persona.Control.Ctrl
     public class CtrlValidaInjection
     {
         private readonly Regex sqlInjectionRegex = new Regex(@"('(''|[^'])*')|(\b(ALTER|alter|Alter|CREATE|create|Create|DELETE|delete|Delete|DROP|drop|Drop|EXEC(UTE){0,1}|exec(ute){0,1}|Exec(ute){0,1}|INSERT( +INTO){0,1}|insert( +into){0,1}|Insert( +into){0,1}|MERGE|merge|Merge|SELECT|Select|select|UPDATE|update|Update|UNION( +ALL){0,1}|union( +all){0,1}|Union( +all){0,1})\b)");
-        private readonly Regex htmlInjectionRegex = new Regex(@"<\s*([^ >]+)[^>]*>.*?<\s*/\s*\1\s*>");
+        private readonly Regex htmlInjectionRegex = new Regex(@"<.*?>|&.*?;");
         public bool sqlInjectionValida(String _informacion, ref String _mensaje, String _etiquetaReferente, ref System.Web.UI.WebControls.TextBox _control)
         {
             bool isMatch = this.sqlInjectionRegex.IsMatch(_informacion);
